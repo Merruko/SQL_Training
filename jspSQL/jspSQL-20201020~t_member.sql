8f3fb5b096be25fb640583699ccdc64ebf065476
KIM DAEHO <mrkim9427@naver.com>
2020-11-14 18:31:36 +0900

    SQL_Training

--- jspSQL/jspSQL-20201020~t_member.sql
+++ jspSQL/jspSQL-20201020~t_member.sql
@@ -1,9 +1,9 @@
 -- 테이블 삭제
 DROP TABLE t_employee;
 
--- 테이블 추가
+-- 테이블 생성
 CREATE TABLE t_member (
-    memberId      number(4) PRIMARY KEY,
+    memberId      NUMBER(4) PRIMARY KEY,
     passwd        VARCHAR2(20),
     name          VARCHAR2(20),
     gender        VARCHAR2(4),
@@ -14,7 +14,7 @@ CREATE TABLE t_member (
 DESC t_member;
 
 -- 데이터 추가
-INSERT INTO t_member VALUES (9427, '0319', '김머호', '남', SYSDATE);
+INSERT INTO t_member VALUES (9427, '0319', '김대호', '남', SYSDATE);
 
 -- 테이블 조회
 SELECT * FROM t_member;
@@ -34,7 +34,7 @@ ALTER TABLE t_member DROP COLUMN joinDate;
 
 -- 내용 수정
 UPDATE t_member
-SET NAME = '김머호'
+SET name = '김머호'
 WHERE memberId = 9427;
 
 -- 모든 자료 삭제
