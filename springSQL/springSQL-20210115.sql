02cc1a4972cc8ca2acd815154a5e2bbcabc676b2
KIM DAEHO <mrkim9427@naver.com>
2021-01-31 19:24:38 +0900

    SQL_Training

--- springSQL/springSQL-20210115.sql
+++ springSQL/springSQL-20210115.sql
@@ -17,7 +17,7 @@ CREATE TABLE s_board (
     cnt NUMBER(5) DEFAULT 0
 );
 
-INSERT INTO s_member VALUES ('kdh9427','0319','김대호','남','19930329','mrkim9427@naver.com','01095059427');
+INSERT INTO s_member VALUES ('kdh9427','0319','관리자','Admin','19930329','mrkim9427@naver.com','01095059427');
 INSERT INTO s_board (seq,title,writer,content) VALUES (1,'임시 공지사항','관리자','이래라저래라');
 
 SELECT * FROM s_member;
